6373f00706d61711b4884ac74d5a3f6609f56818
klchib3 <klchib3@gmail.com>
2022-08-20 15:46:01 +0800

    landing

--- landing/src/test/java/com/hanye/info/AbstractTest.java
+++ landing/src/test/java/com/hanye/info/AbstractTest.java
@@ -3,6 +3,7 @@ package com.hanye.info;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import com.hanye.info.service.SeminarService;
+import com.hanye.info.model.mongo.MongoSequence;
 import com.hanye.info.service.MailJobService;
 
 @SpringBootTest
@@ -14,6 +15,9 @@ public class AbstractTest {
 	
 	@Autowired
 	protected SeminarService seminarService;
+	
+	@Autowired
+	protected MongoSequence mongoSequence;
 	
 //	@Autowired
 //	protected MockMvc mockMvc;
